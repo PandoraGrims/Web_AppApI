3eb1dbf323aabdd3c8ebeca116afe22e06126667
Pandora <nimfanefor@gmail.com>
2024-02-02 17:31:51 +0600

    измени юрл (задался вопросом зачем прописывать name=)

--- webapp/webapp_api/urls.py
+++ webapp/webapp_api/urls.py
@@ -6,8 +6,8 @@ app_name = "webapp_api"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("api_v1/add/", view_add, name="view_add"),
-    path("api_v1/subtract/", view_subtract, name="view_subtract"),
-    path("api_v1/multiply/", view_multiply, name="view_multiply"),
-    path("api_v1/divide/", view_divide, name="view_divide"),
+    path("api_v1/add/", view_add, name="view_add_api"),
+    path("api_v1/subtract/", view_subtract, name="view_subtract_api"),
+    path("api_v1/multiply/", view_multiply, name="view_multiply_api"),
+    path("api_v1/divide/", view_divide, name="view_divide_api"),
 ]
